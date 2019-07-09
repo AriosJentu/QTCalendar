49ce9d3c483bce5f9e21a46b0dca7f41ce116030
AriosJentu <darthpoezd@gmail.com>
2019-07-09 16:39:24 +1000

    Fix event list for day, add function to post event to server

--- src/server.js
+++ src/server.js
@@ -13,8 +13,9 @@ function encodeQueryData(data) {
    return "?"+ret.join('&');
 }
 
-function getVisibilityForDate(inputdate, afterfunc) {
+function getVisibilityForDate(inputdate, afterfunc, errorfunc) {
 
+    afterfunc(0);
     var request = new XMLHttpRequest();
 
     var date = inputdate;
@@ -32,7 +33,8 @@ function getVisibilityForDate(inputdate, afterfunc) {
                 var jsonRes = JSON.parse(request.responseText);
                 afterfunc(jsonRes.count);
             } else {
-                console.log("HTTP Request failed", request.status);
+                console.log("Error in Event Instances GET Request");
+                errorfunc(request);
             }
         }
     }
@@ -42,7 +44,7 @@ function getVisibilityForDate(inputdate, afterfunc) {
     request.send();
 }
 
-function getEventsForDate(inputdate, updatefunc) {
+function getEventsForDate(inputdate, updatefunc, errorfunc) {
 
     var array = [];
     updatefunc(array);
@@ -59,6 +61,7 @@ function getEventsForDate(inputdate, updatefunc) {
     var url = S_INSTANCES+dat;
 
     request.onreadystatechange = function() {
+        array = []
         updatefunc(array);
 
         if (request.readyState === 4) {
@@ -66,49 +69,62 @@ function getEventsForDate(inputdate, updatefunc) {
 
                 var jsonData = JSON.parse(request.responseText).data;
 
-                for (var i = 0; i < jsonData.length; i++) {
+                var identifs = [];
+                var identsByID = {}
+                for (let i = 0; i < jsonData.length; i++) {
+                    identifs.push("id=" + jsonData[i].event_id);
+                    identsByID[jsonData[i].event_id] = i;
+                }
 
-                    var element = jsonData[i];
+                var requestEvents = new XMLHttpRequest();
+                var url = S_EVENTS+"?"+identifs.join("&");
 
-                    var dataArray = {};
-                    var starttime = Number(element.started_at);
-                    var endtime = Number(element.ended_at);
+                requestEvents.onreadystatechange = function() {
+                    if (requestEvents.readyState === 4) {
+                        if (requestEvents.status === 200) {
 
-                    dataArray.id = element.event_id;
-                    dataArray.patrnid = element.pattern_id;
-                    dataArray.startTime = new Date(starttime);
-                    dataArray.endTime = new Date(endtime);
 
-                    var nrequest = new XMLHttpRequest();
-                    var nurl = S_EVENT_ID+dataArray.id;
+                            var jsonEventsData = JSON.parse(requestEvents.responseText).data;
 
-                    nrequest.onreadystatechange = function() {
-                        if (nrequest.readyState === 4) {
-                            if (nrequest.status === 200) {
+                            for (let i = 0; i < jsonEventsData.length; i++) {
 
-                                var njsonData = JSON.parse(nrequest.responseText).data[0];
+                                var elementEvent = jsonEventsData[i];
+                                var element = jsonData[ identsByID[elementEvent.id] ];
 
-                                dataArray.name = njsonData.name;
-                                dataArray.details = njsonData.details;
-                                dataArray.owner = njsonData.owner_id;
-                                dataArray.location = njsonData.location;
+                                var starttime = Number(element.started_at);
+                                var endtime = Number(element.ended_at);
 
-                                if (!(dataArray in array)) {
-                                    array.push(dataArray);
-                                }
+                                var eventData = {};
+                                eventData.id = elementEvent.id;
+                                eventData.patrnid = element.pattern_id;
+                                eventData.startTime = new Date(starttime);
+                                eventData.endTime = new Date(endtime);
 
+                                eventData.name = elementEvent.name;
+                                eventData.details = elementEvent.details;
+                                eventData.owner = elementEvent.owner_id;
+                                eventData.location = elementEvent.location;
+                                eventData.status = elementEvent.status;
+
+                                array.push(eventData);
                                 updatefunc(array);
                             }
+
+                        } else {
+
+                            console.log("Error in Event Elements GET Request");
+                            errorfunc(request);
                         }
                     }
-
-                    nrequest.open("GET", nurl);
-                    nrequest.setRequestHeader(AUTH_NAME, AUTH_TOKEN);
-                    nrequest.send();
-
                 }
+
+                requestEvents.open("GET", url);
+                requestEvents.setRequestHeader(AUTH_NAME, AUTH_TOKEN);
+                requestEvents.send()
+
             } else {
-                console.log("HTTP Request failed", request.readyState, request.status);
+                console.log("Error in Event Instances GET Request");
+                errorfunc(request);
             }
         }
     }
@@ -116,5 +132,92 @@ function getEventsForDate(inputdate, updatefunc) {
     request.open("GET", url);
     request.setRequestHeader(AUTH_NAME, AUTH_TOKEN);
     request.send();
+}
 
+function postEventToServer(event, afterfunc, errorfunc) {
+
+    var jsonForEvent = {};
+    jsonForEvent.details = event.details;
+    jsonForEvent.location = event.location;
+    jsonForEvent.name = event.name;
+    jsonForEvent.status = "";
+    var evtJsonString = JSON.stringify(jsonForEvent);
+
+    var requestEvent = new XMLHttpRequest();
+
+    requestEvent.onreadystatechange = function() {
+
+        if (requestEvent.readyState === 4) {
+            if (requestEvent.status === 200) {
+
+                var createdEventID = JSON.parse(requestEvent.responseText).data[0].id;
+
+                var jsonForPattern = {}
+                jsonForPattern.started_at = event.startTime.getTime();
+                jsonForPattern.ended_at = event.endTime.getTime();
+                jsonForPattern.timezone = "UTC";
+                var patJsonString = JSON.stringify(jsonForPattern);
+
+                var url = S_PATTERNS+encodeQueryData({"event_id":createdEventID});
+
+                var requestPattern = new XMLHttpRequest();
+
+                requestPattern.onreadystatechange = function() {
+
+                    if (requestPattern.readyState === 4) {
+                        if (requestPattern.status === 200) {
+                            afterfunc();
+                        } else {
+                            console.log("Error in Pattern POST Request");
+                            errorfunc(requestPattern);
+                        }
+                    }
+                }
+
+                console.log(patJsonString);
+
+                requestPattern.open("POST", url);
+                requestPattern.setRequestHeader(AUTH_NAME, AUTH_TOKEN);
+                requestPattern.setRequestHeader("Content-Type", "application/json");
+                requestPattern.send(patJsonString);
+
+            } else {
+                console.log("Error in Event POST Request");
+                errorfunc(requestEvent);
+            }
+        }
+    }
+
+
+    requestEvent.open("POST", S_EVENTS);
+    requestEvent.setRequestHeader(AUTH_NAME, AUTH_TOKEN);
+    requestEvent.setRequestHeader("Content-Type", "application/json");
+    requestEvent.send(evtJsonString);
+}
+
+function generateRandomEvent(inputdate) {
+
+    var event = {};
+
+    var randstart = Math.floor(Math.random()*23);
+    var randend = Math.floor(Math.random()*(23-randstart)) + randstart;
+
+    var date = inputdate;
+    date.setHours(randstart, 0, 0, 0);
+    var start = date.getTime();
+    date.setHours(randend, 59, 59, 999);
+    var ends = date.getTime();
+
+    event.startTime = new Date(start);
+    event.endTime = new Date(ends);
+    event.name = "[AJ] Test Event With Random Number " + Math.floor(Math.random()*1500);
+    event.details = "[AJ] Test Event Details With Random Number " + Math.floor(Math.random()*1500);
+    event.owner = "AriosJentu";
+    event.location = "Vladivostok";
+
+    return event;
+}
+
+function basicErrorFunc(request) {
+    console.log("HTTP Request failed", request.readyState, request.status);
 }
